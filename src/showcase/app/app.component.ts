ea78da3d00b4bfd1387659f13fc87d646c2c9b2c
JoonYoung Lee <kuroneko0441@gmail.com>
2019-02-16 19:36:45 +0900

    Initial commit

---
+++ src/showcase/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'kns-app',
+    templateUrl: './app.component.html',
+    styleUrls: [ './app.component.scss' ],
+})
+export class AppComponent {}
